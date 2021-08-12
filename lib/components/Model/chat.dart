3c79f800582e9824102dfce3d46e9123bbfdb283
Krunal Sutariya <krunal.exaltaretech@gmail.com>
2021-08-12 16:45:24 +0530

    chat implemented

---
+++ lib/components/Model/chat.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+
+import 'package:intl/intl.dart';
+import 'package:video_chat/app/utils/date_utils.dart';
+import 'package:video_chat/components/Model/message_obj.dart';
+
+ChatObj chatFromJson(String str) => ChatObj.fromJson(json.decode(str));
+
+String chatToJson(ChatObj data) => json.encode(data.toJson());
+
+class ChatObj {
+  static String _kChatDate = "ChatDate";
+  static String _kChatMessages = "ChatMessages";
+  ChatObj({
+    this.chatDate,
+    this.messageObjList,
+  });
+
+  DateTime chatDate;
+  List<MessageObj> messageObjList;
+
+  factory ChatObj.fromJson(Map<String, dynamic> json) => ChatObj(
+        chatDate: json[_kChatDate] ?? "",
+        messageObjList: json[_kChatMessages] == null
+            ? []
+            : List<MessageObj>.from(
+                json[_kChatMessages].map((x) => MessageObj.fromJson(x))),
+      );
+
+  Map<String, dynamic> toJson() => {
+        _kChatDate: chatDate,
+        _kChatMessages:
+            List<dynamic>.from(messageObjList.map((x) => x.toJson())),
+      };
+
+  // Get chating dates...
+  String get getChatingDates => DateUtilities().getFormattedDay(chatDate);
+}
