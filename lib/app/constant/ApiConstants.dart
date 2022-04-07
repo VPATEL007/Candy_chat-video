1a5332660be564f686e89243b3872975ea500662
Mayur Amipara <mayur.amipara@exaltaretech.com>
2022-04-07 11:47:57 +0530

    conflict

--- lib/app/constant/ApiConstants.dart
+++ lib/app/constant/ApiConstants.dart
@@ -62,8 +62,7 @@ class ApiConstants {
   static const String paymentMethod = "payment-method/get-all";
   static const String withDrawRequestList =
       "transaction/app-withdrawal-requests";
-       static const String verifyFace =
-      "profile/face-verification";
+  static const String verifyFace = "profile/face-verification";
 
   static const String invitationVerification = "profile/verify-invitation";
 
@@ -71,5 +70,4 @@ class ApiConstants {
       "dashboard/get-top-weekly-infulencer";
   static const String influeencerEarningReport =
       "dashboard/get-earning-report-infulencer";
-  static const String verifyFace = "profile/face-verification";
 }
